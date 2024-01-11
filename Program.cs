11a3cb9f2ba99a6b727f6963dd24572761656943
ALLUK <lam83@bk.ru>
2024-01-11 23:54:14 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point pointOne=new Point();
+            Console.WriteLine(pointOne.y);
+
+            Console.ReadLine();
+        }
+    }
+    class Point
+    {
+        private int x = 5;
+        public int y = 4;
+
+        public void PrintX()
+        {
+            Console.WriteLine("X: "+ x);
+        }
+        private void PrintY()
+        {
+            Console.WriteLine("Y: "+ y);
+        }
+        public void PrintPoint()
+        {
+            Console.WriteLine("X: "+ x);
+            Console.WriteLine("Y: "+ y);
+        }
+    }
+}
